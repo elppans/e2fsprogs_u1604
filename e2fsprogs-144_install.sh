9a96e77cc1559a2d282761782748a3759888244a
elppans <elppansmk@gmail.com>
2024-10-22 11:45:51 -0300

    Moduficado linhas dpkg

--- e2fsprogs-144_install.sh
+++ e2fsprogs-144_install.sh
@@ -7,9 +7,13 @@
 # http://cz.archive.ubuntu.com/ubuntu/pool/main/e/e2fsprogs/libcomerr2_1.44.1-1_amd64.deb
 # http://cz.archive.ubuntu.com/ubuntu/pool/main/e/e2fsprogs/libext2fs2_1.44.1-1_amd64.deb
 
-sudo dpkg -i libcom-err2_1.44.1-1_amd64.deb
-sudo dpkg -i libcomerr2_1.44.1-1_amd64.deb
-sudo dpkg -i libext2fs2_1.44.1-1_amd64.deb
-sudo dpkg -i e2fslibs_1.44.1-1_amd64.deb
+# sudo dpkg -i libcom-err2_1.44.1-1_amd64.deb
+# sudo dpkg -i libcomerr2_1.44.1-1_amd64.deb
+# sudo dpkg -i libext2fs2_1.44.1-1_amd64.deb
+# sudo dpkg -i e2fslibs_1.44.1-1_amd64.deb
+# sudo dpkg -i e2fsprogs_1.44.1-1_amd64.deb
+
+sudo dpkg -i libcom-err2_1.44.1-1_amd64.deb libcomerr2_1.44.1-1_amd64.deb libext2fs2_1.44.1-1_amd64.deb e2fslibs_1.44.1-1_amd64.deb
 sudo dpkg -i e2fsprogs_1.44.1-1_amd64.deb
+
 dpkg -l e2fsprogs
