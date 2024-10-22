3eb3ad566c95c9f22a396d28eb38fb26a13dc5a1
elppans <elppansmk@gmail.com>
2024-10-22 11:28:56 -0300

    Adicionado sudo nas linhas

--- e2fsprogs-144_install.sh
+++ e2fsprogs-144_install.sh
@@ -7,9 +7,9 @@
 # http://cz.archive.ubuntu.com/ubuntu/pool/main/e/e2fsprogs/libcomerr2_1.44.1-1_amd64.deb
 # http://cz.archive.ubuntu.com/ubuntu/pool/main/e/e2fsprogs/libext2fs2_1.44.1-1_amd64.deb
 
-dpkg -i libcom-err2_1.44.1-1_amd64.deb
-dpkg -i libcomerr2_1.44.1-1_amd64.deb
-dpkg -i libext2fs2_1.44.1-1_amd64.deb
-dpkg -i e2fslibs_1.44.1-1_amd64.deb
-dpkg -i e2fsprogs_1.44.1-1_amd64.deb
+sudo dpkg -i libcom-err2_1.44.1-1_amd64.deb
+sudo dpkg -i libcomerr2_1.44.1-1_amd64.deb
+sudo dpkg -i libext2fs2_1.44.1-1_amd64.deb
+sudo dpkg -i e2fslibs_1.44.1-1_amd64.deb
+sudo dpkg -i e2fsprogs_1.44.1-1_amd64.deb
 dpkg -l e2fsprogs
